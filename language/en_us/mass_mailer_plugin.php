3b26318a23485d6f6cfb3ed5a0796384c31f7824
CyanDark, Incorporated <admin@cyandark.com>
2015-11-23 21:58:04 -0600

    ES Language Added
    ES Language Added

--- language/en_us/mass_mailer_plugin.php
+++ language/en_us/mass_mailer_plugin.php
@@ -1,3 +1,9 @@
 <?php
 $lang['MassMailerPlugin.admin_forms.name'] = "Mass Mailer";
+$lang['MassMailerPlugin.admin_forms.from_name'] = "From Name";
+$lang['MassMailerPlugin.admin_forms.from_email'] = "From Email";
+$lang['MassMailerPlugin.admin_forms.subject'] = "Subject";
+$lang['MassMailerPlugin.admin_forms.HTML'] = "HTML";
+$lang['MassMailerPlugin.admin_forms.text'] = "Text";
+$lang['MassMailerPlugin.admin_forms.send_email'] = "Send Email";
 ?>
---
+++ language/es_es/mass_mailer_plugin.php
@@ -0,0 +1,9 @@
+<?php
+$lang['MassMailerPlugin.admin_forms.name'] = "Mass Mailer";
+$lang['MassMailerPlugin.admin_forms.from_name'] = "Nombre de Remitente";
+$lang['MassMailerPlugin.admin_forms.from_email'] = "Correo de Remitente";
+$lang['MassMailerPlugin.admin_forms.subject'] = "Asunto";
+$lang['MassMailerPlugin.admin_forms.HTML'] = "HTML";
+$lang['MassMailerPlugin.admin_forms.text'] = "Text";
+$lang['MassMailerPlugin.admin_forms.send_email'] = "Enviar Correo";
+?>
